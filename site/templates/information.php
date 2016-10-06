49a99c89b9fe9d618893f70ee43326f15435e607
lodewijk <lodewijk@waag.org>
2016-10-06 23:09:41 +0200

    content for downloads

--- site/templates/information.php
+++ site/templates/information.php
@@ -4,7 +4,7 @@
     <?php foreach($page->children()->visible() as $info): ?>
         <div class="information__block flex flex__wrap flex__center">
           <figure class="full__height half__width flex flex__center">
-            <img src="<?php echo url(); ?>/content/<?php echo $info->diruri() ?>/<?php echo html($info->infoimage()) ?>" alt="<?php echo $info->title()->html() ?> image">
+            <img src="<?php echo $info->image($info->infoimage())->url() ?>" alt="<?php echo $info->title()->html() ?> image">
           </figure>
           <div class="half__width text__block flex flex__center infotje">
             <div>
@@ -18,7 +18,7 @@
               </header>
               <div class="intro">
                 <?php echo $info->text()->kirbytext() ?>
-                <a href="#" class="download__btn"><span class="download__icon"></span> <?php echo $info->title()->html() ?></a>
+                <a href="<?php echo $info->document($info->download())->url() ?>" class="download__btn"><span class="download__icon"></span> <?php echo $info->title()->html() ?></a>
               </div>
             </div>
           </div>
