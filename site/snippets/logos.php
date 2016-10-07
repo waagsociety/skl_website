7abebd7a2eee2fcfe5608dbaeadc9d6aa783d92c
Martin Risseeuw <martin@martinr.nl>
2016-10-07 16:27:10 +0200

    svg logos partners

--- site/snippets/logos.php
+++ site/snippets/logos.php
@@ -1,8 +1,8 @@
 <ul class="logo__party">
-  <li class="item"><a href="https://ec.europa.eu/programmes/horizon2020/"><img src="<?php echo url('assets/images/eu.jpg') ?>" /></a></li>
-  <li class="item"><a href="http://making-sense.eu/"><img src="<?php echo url('assets/images/makingsense.jpg') ?>" /></a></li>
-  <li class="item"><a href="https://waag.org"><img src="<?php echo url('assets/images/waag.jpg') ?>" /></a></li>
-  <li class="item"><a href="https://www.cinekid.nl/"><img src="<?php echo url('assets/images/cinekid.jpg') ?>" /></a></li>
-  <li class="item"><a href="http://stichtingsec.nl/?lang=en"><img src="<?php echo url('assets/images/sec.jpg') ?>" /></a></li>
-  <li class="item"><a href="http://www.rivm.nl/"><img src="<?php echo url('assets/images/rijks.jpg') ?>" /></a></li>
+  <li class="item"><a href="https://ec.europa.eu/programmes/horizon2020/"><img src="<?php echo url('assets/images/logo_eu.svg') ?>" /></a></li>
+  <li class="item"><a href="http://making-sense.eu/"><img src="<?php echo url('assets/images/logo_makingsense.svg') ?>" /></a></li>
+  <li class="item"><a href="https://waag.org"><img src="<?php echo url('assets/images/logo_waag.svg') ?>" /></a></li>
+  <li class="item"><a href="https://www.cinekid.nl/"><img src="<?php echo url('assets/images/logo_cinekid.svg') ?>" /></a></li>
+  <li class="item"><a href="http://stichtingsec.nl/?lang=en"><img src="<?php echo url('assets/images/logo_sec.svg') ?>" /></a></li>
+  <li class="item"><a href="http://www.rivm.nl/"><img src="<?php echo url('assets/images/logo_rivm.svg') ?>" /></a></li>
 </ul>
