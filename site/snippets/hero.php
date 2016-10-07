b515a8e98f8fda784dfc9a1d682356b8c509db18
Martin Risseeuw <martin@martinr.nl>
2016-10-07 11:15:51 +0200

    style update responsiveness

--- site/snippets/hero.php
+++ site/snippets/hero.php
@@ -7,7 +7,7 @@
         <h1>Smart kids lab</h1>    
         <p class="tagline">Meters maken</p>
       </div>
-      <p class="introdescription">Hoe schoon is de LUCHT die je inademt? Waarom is zwemWATER geen drinkwater? Zitten er veel of weinig microben in de GROND? En wat betekent dat? Ontdek hoe gezond jouw buurt is en wat je zelf kan doen om het te verbeteren.</p>
+      <p class="introdescription">Hoe schoon is de LUCHT die je inademt? Waarom is zwemWATER geen drinkwater? Zitten er veel of weinig microben in de GROND? En wat betekent dat? Ontdek hoe gezond jouw buurt is en wat je zelf kan doen om het te verbeteren. Met het Smart Kids Lab kun je water, geluid, lucht, aarde en licht onderzoeken met zelfgemaakte meetinstrumenten. HOE WERKT HET? Kies wat je wilt gaan onderzoeken in jouw buurt. Download de instructies en ga aan de slag.</p>
     </div>
     <div class="decoration rightdec"></div>
   </div>
--- site/templates/default.php
+++ site/templates/default.php
@@ -1,8 +1,13 @@
 <?php snippet('header') ?>
-  <main class="main" role="main">
-    <div class="text">
-      <h1><?php echo $page->title()->html() ?></h1>
-      <?php echo $page->text()->kirbytext() ?>
+<?php snippet('navigation') ?>
+<header class="hero__header full__width flex flex__wrap">
+  <div class="introduction default full__width text__center flex flex__center">
+    <div class="decoration leftdec"></div>
+    <div class="intro__inner">
+      <p class="introdescription">Hoe schoon is de LUCHT die je inademt? Waarom is zwemWATER geen drinkwater? Zitten er veel of weinig microben in de GROND? En wat betekent dat? Ontdek hoe gezond jouw buurt is en wat je zelf kan doen om het te verbeteren. Met het Smart Kids Lab kun je water, geluid, lucht, aarde en licht onderzoeken met zelfgemaakte meetinstrumenten. HOE WERKT HET? Kies wat je wilt gaan onderzoeken in jouw buurt. Download de instructies en ga aan de slag.</p>
     </div>
-  </main>
+    <div class="decoration rightdec"></div>
+  </div>
+</header>
+
 <?php snippet('footer') ?>
