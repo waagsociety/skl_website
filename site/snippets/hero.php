ddb127128817b8b70b208315cab774e5b86099b3
lodewijk <lodewijk@waag.org>
2016-10-10 11:21:48 +0200

    changed hero content and styles, header height

--- site/snippets/hero.php
+++ site/snippets/hero.php
@@ -1,17 +1,16 @@
 <header class="hero__header full__width flex flex__wrap">
-  <div class="introduction full__width text__center flex flex__center">
-    <div class="decoration leftdec"></div>
-    <div class="intro__inner">
-      <div class="logo_text">
-        <h1>Smart kids lab</h1>    
-        <p class="tagline">Meters maken</p>
-      </div>
-      <p class="introdescription">Hoe schoon is de LUCHT die je inademt? Waarom is zwemWATER geen drinkwater? Zitten er veel of weinig microben in de GROND? En wat betekent dat? Ontdek hoe gezond jouw buurt is en wat je zelf kan doen om het te verbeteren. Met het Smart Kids Lab kun je water, geluid, lucht, aarde en licht onderzoeken met zelfgemaakte meetinstrumenten. HOE WERKT HET? Kies wat je wilt gaan onderzoeken in jouw buurt. Download de instructies en ga aan de slag.</p>
-    </div>
-    <div class="decoration rightdec"></div>
-  </div>
-  <div class="wrapper">
-    <a href="<?php echo $site->url() ?>/maak-een-meter" class="half__width home__icon meter__maken"></a>
-    <a href="<?php echo $site->url() ?>/resultaten" class="half__width home__icon meet__delen"></a>
-  </div>
+    <div class="introduction full__width text__center flex flex__center"> 
+	   <p class="introdescription">Met het Smart Kids Lab kun je water, geluid, lucht, aarde en licht onderzoeken met zelfgemaakte meetinstrumenten.
+	   </p>
+	</div>
+  
+  	<div class="wrapper">
+	    <a href="<?php echo $site->url() ?>/maak-een-meter" class="half__width home__icon meter__maken"></a>
+	    <a href="<?php echo $site->url() ?>/resultaten" class="half__width home__icon meet__delen"></a>
+	</div> 
+	
+	<div class="introduction full__width text__center flex flex__center"> 
+		<p class="introdescription">Hoe schoon is de LUCHT die je inademt? Waarom is zwemWATER geen drinkwater? Zitten er veel of weinig microben in de GROND? En wat betekent dat? Ontdek hoe gezond jouw buurt is en wat je zelf kan doen om het te verbeteren. <br><br> HOE WERKT HET? Kies wat je wilt gaan onderzoeken <a style="color: black;text-decoration:underline;" href="/maak-een-meter">MAAK EEN METER</a> en ga aan de slag.
+	   </p>
+	</div>                                                                                                 
 </header>
