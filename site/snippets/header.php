bb4a2f71b8c18e1b3b4428955135483638571b74
lodewijk <lodewijk@waag.org>
2016-10-18 14:32:37 +0200

    new downloads, css popups

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -2,7 +2,7 @@
 <html lang="en">
 <head>
   <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0">
   <title><?php echo $site->title()->html() ?> | <?php echo $page->title()->html() ?></title>
   <meta name="description" content="<?php echo $site->description()->html() ?>">
   <meta name="keywords" content="<?php echo $site->keywords()->html() ?>">
