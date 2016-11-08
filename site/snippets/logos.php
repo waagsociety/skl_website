561928c5c5850bf3ff2acb18fd2ad8bb42d7d336
lodewijk <lodewijk@waag.org>
2016-11-08 16:56:34 +0100

    more logos

--- site/snippets/logos.php
+++ site/snippets/logos.php
@@ -5,4 +5,6 @@
   <li class="item"><a href="https://www.cinekid.nl/"><img src="<?php echo url('assets/images/logo_cinekid.svg') ?>" /></a></li>
   <li class="item"><a href="http://stichtingsec.nl/"><img src="<?php echo url('assets/images/logo_sec.svg') ?>" /></a></li>
   <li class="item"><a href="http://www.rivm.nl/"><img src="<?php echo url('assets/images/logo_rivm.svg') ?>" /></a></li>
+  <li class="item"><a href="http://www.amsterdam.nl/"><img src="<?php echo url('assets/images/logo_ams.svg') ?>" /></a></li>
+  <li class="item"><a href="http://www.thethingsnetwork.org/"><img src="<?php echo url('assets/images/logo_ttn.svg') ?>" /></a></li>
 </ul>
